3e25e16575ddfc5782c80399788ee2369ac1ecbf
Marvin Dawson <marvintdawson@gmail.com>
2020-05-17 05:30:26 -0500

    fix page structure and added all content

---
+++ README.md
@@ -0,0 +1,3 @@
+#Learn More VROM
+
+This page is an introductory to GetVROM.com request service application.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,13 @@ export class HomePage {
 
   headerLogo = '/assets/imgs/logo.png';
   homePageImage = '/assets/imgs/site/front_office2.jpg';
-  customerSatisfactionImage = '/assets/img/site/customer_satisfaction_2.png';
+  customerSatisfactionImage = '/assets/imgs/site/customer_satisfaction_2.png';
+  siteName = 'VROM';
+  siteFooterName = 'VROM LLC';
+  siteTagLine = 'Manage Anywhere...';
+  valueProposition = 'Capture Data Build Perception Increase Profits';
+  valuePropositionFooter = 'Capture Data -> Build Perception -> Increase Profits';
+  date = new Date().getFullYear();
   constructor() {}
 
   openFMLModal() {}
