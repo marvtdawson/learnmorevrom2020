7d87ab0d4ecc8e3716fe2d2c70052eed96239130
Marvin Dawson <marvintdawson@gmail.com>
2020-05-17 02:10:07 -0500

    first initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+// tslint:disable-next-line:component-class-suffix
 export class HomePage {
 
+  headerLogo = '/assets/imgs/logo.png';
+  homePageImage = '/assets/imgs/site/front_office2.jpg';
+  customerSatisfactionImage = '/assets/img/site/customer_satisfaction_2.png';
   constructor() {}
+
+  openFMLModal() {}
 
 }
